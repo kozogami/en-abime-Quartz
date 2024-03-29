e9b865cebe950e5b9e32671e1691b94a798a0992
kozogami <153994577+kozogami@users.noreply.github.com>
2024-03-29 04:03:31 -0700

    Quartz sync: Mar 29, 2024, 4:03 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,8 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Quartz Discord Community": "https://discord.gg/cRFFHYye7t",
+	  "Tati's Discord (En Abime)": "https://discord.gg/BAUbGc5a"
     },
   }),
 }
