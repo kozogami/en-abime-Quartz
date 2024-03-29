a6a7e278248712d50ea8874aef2d79fff400c50e
kozogami <153994577+kozogami@users.noreply.github.com>
2024-03-29 04:27:06 -0700

    Quartz sync: Mar 29, 2024, 4:27 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  pageBody: Component.Graph(globalGraph),
+  pageBody: Component.Graph(),
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
